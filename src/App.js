a99f056dc21a173b340f74e67a45db2b8499985b
rootMa99 <anasszeroual09@gmail.com>
2024-04-22 20:35:08 +0100

    doly

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
-import './App.css';
-import NavBar from './components/ui/NavBar';
+import "./App.css";
+import TeamLeaderCard from "./components/home/TeamLeaderCard";
+import NavBar from "./components/ui/NavBar";
 
 function App() {
   return (
     <div className="App">
       <NavBar />
+      <div className="teamLeaders">
+        <TeamLeaderCard inver={true} />
+        <TeamLeaderCard />
+      </div>
     </div>
   );
 }
--- src/components/home/TeamLeaderCard.js
+++ src/components/home/TeamLeaderCard.js
@@ -1,16 +1,19 @@
 import c from "./TeamLeaderCard.module.css";
+import me from "../../assets/me.jpeg";
 
-
-const TeamLeaderCard=p=>{
-
-
-
-    return(
-        <div className={c.container}>
-        <div className={c.imgContainer}></div>
-        <div className={c.dataContainer}></div>
-        </div>
-    )
-}
+const TeamLeaderCard = (p) => {
+  return (
+    <div className={c.container} style={p.inver? {} :{'flex-direction': 'row-reverse'}}>
+      <img
+        src={me}
+        className={p.inver ? c.imgContainer : c.imgContainerinv}
+        alt="aptiv"
+        draggable="false"
+      />
+        <h1 className={p.inver ? c.title : c.titleinv}>Anass Zeroual</h1>
+      <div className={p.inver ? c.dataContainer : c.dataContainerinver}></div>
+    </div>
+  );
+};
 
 export default TeamLeaderCard;
