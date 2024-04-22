405ecacfa7d24075056aff121001e405c509b6eb
rootMa99 <anasszeroual09@gmail.com>
2024-04-22 19:48:52 +0100

    let"s see

---
+++ src/components/home/TeamLeaderCard.js
@@ -0,0 +1,16 @@
+import c from "./TeamLeaderCard.module.css";
+
+
+const TeamLeaderCard=p=>{
+
+
+
+    return(
+        <div className={c.container}>
+        <div className={c.imgContainer}></div>
+        <div className={c.dataContainer}></div>
+        </div>
+    )
+}
+
+export default TeamLeaderCard;
