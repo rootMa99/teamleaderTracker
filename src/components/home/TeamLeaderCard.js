c096216b354cb3dcd4fc8072859e52a528a48b76
anass zeroual <anasszeroual09@gmail.com>
2024-04-23 09:19:01 +0100

    tepol

--- src/components/home/TeamLeaderCard.js
+++ src/components/home/TeamLeaderCard.js
@@ -16,7 +16,41 @@ const TeamLeaderCard = (p) => {
       <h1 className={p.inver ? c.title : c.titleinv}>naji mohamed</h1>
       <div className={p.inver ? c.dataContainer : c.dataContainerinver}>
         <div className={c.ty}>
-          <h3>efficiency</h3>
+          <h3>efficiency:</h3>
+          <div className={c.data}>
+            <div className={c.ind}>
+              <span>actual:</span>
+              <p>86%</p>
+            </div>
+            <div className={c.ind}>
+              <span>target:</span>
+              <p>91%</p>
+            </div>
+            <div className={c.ind}>
+              <span>gap:</span>
+              <p>-5%</p>
+            </div>
+          </div>
+        </div>
+        <div className={c.ty}>
+          <h3>head count:</h3>
+          <div className={c.data}>
+            <div className={c.ind}>
+              <span>actual:</span>
+              <p>86%</p>
+            </div>
+            <div className={c.ind}>
+              <span>target:</span>
+              <p>91%</p>
+            </div>
+            <div className={c.ind}>
+              <span>gap:</span>
+              <p>-5%</p>
+            </div>
+          </div>
+        </div>
+        <div className={c.ty}>
+          <h3>output</h3>
           <div className={c.data}>
             <div className={c.ind}>
               <span>actual:</span>
