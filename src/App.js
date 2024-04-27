72549dc18030bb168ee4f77efdc5355f4afe7879
rootMa99 <anasszeroual09@gmail.com>
2024-04-27 13:10:16 +0100

    frezland

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import "./App.css";
 import Chart from "./components/home/Chart";
+import MyDropzone from "./components/home/MyDropzone";
 import StackedBarChart from "./components/home/StackedBarChart";
 import NavBar from "./components/ui/NavBar";
 
@@ -11,20 +12,30 @@ function App() {
     { label: "D", value: 25 },
   ];
   const dataf = [
-    { label: 'A', series1: 100, series2: 20, series3: 30 },
-    { label: 'B', series1: 15, series2: 25, series3: 35 },
-    { label: 'BC', series1: 11, series2: 35, series3: 25 },
-    { label: 'BD', series1: 17, series2: 5, series3: 3 },
+    { label: "A", series1: 100, series2: 20, series3: 30 },
+    { label: "B", series1: 15, series2: 25, series3: 35 },
+    { label: "BC", series1: 11, series2: 35, series3: 25 },
+    { label: "BD", series1: 17, series2: 5, series3: 3 },
   ];
   return (
     <div className="App">
       <NavBar />
-      <div style={{ height: "30rem", backgroundColor:"#634747"}}>
+      <div style={{ height: "30rem", backgroundColor: "#634747" }}>
         <Chart data={data} />
       </div>
-      <div style={{ height: "30rem", backgroundColor:"#3d7558"}}>
+      <div style={{ height: "30rem", backgroundColor: "#3d7558" }}>
         <StackedBarChart data={dataf} />
       </div>
+      <MyDropzone onDrop={(acceptedFiles) => console.log(acceptedFiles)}>
+        {({ getRootProps, getInputProps }) => (
+          <section>
+            <div {...getRootProps()}>
+              <input {...getInputProps()} />
+              <p>Drag 'n' drop some files here, or click to select files</p>
+            </div>
+          </section>
+        )}
+      </MyDropzone>
     </div>
   );
 }
