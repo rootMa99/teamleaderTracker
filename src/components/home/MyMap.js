b85f4a8ae15492652b22f6ca45f0bf30eb45ed8c
rootMa99 <anasszeroual09@gmail.com>
2024-04-28 00:06:05 +0100

    retro

--- src/components/home/MyMap.js
+++ src/components/home/MyMap.js
@@ -1,9 +1,19 @@
 import React from "react";
 import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
 import "leaflet/dist/leaflet.css";
- 
+import locationp from "../../assets/location.png"
+import L from 'leaflet';
+
 const MyMap = ({location, error}) => {
   
+  const customIcon = L.icon({
+    iconUrl: locationp, 
+    iconSize: [25, 41], 
+    iconAnchor: [12, 41], 
+    popupAnchor: [0, -41] 
+  });
+
+
   return (
     <div style={{ height: "100%", width: "100%" }}>
       {location ? (
@@ -14,7 +24,7 @@ const MyMap = ({location, error}) => {
         >
         {console.log("you see me", location.lat, location.lng)}
           <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
-          <Marker position={[location.lat, location.lng]}>
+          <Marker position={[location.lat, location.lng]} icon={customIcon}>
             <Popup>You are here</Popup>
           </Marker>
         </MapContainer>
