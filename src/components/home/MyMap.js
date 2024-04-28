24ecdded0a70fa9b28fdb87c198aa36e4bfe7137
rootMa99 <anasszeroual09@gmail.com>
2024-04-28 13:37:41 +0100

    teop

--- src/components/home/MyMap.js
+++ src/components/home/MyMap.js
@@ -1,32 +1,22 @@
 import React, { useState, useEffect } from 'react';
 import { MapContainer, TileLayer, Marker, Popup, useMapEvents, Polyline } from 'react-leaflet';
-import L from 'leaflet'; // Import Leaflet library
-import 'leaflet/dist/leaflet.css'; // Import Leaflet CSS
+import L from 'leaflet'; 
+import 'leaflet/dist/leaflet.css';
 import locationp from "../../assets/location.png"
 
-// Define custom icon for the target marker
+
 const targetIcon = L.icon({
-  iconUrl: locationp, // URL to the custom marker icon
-  iconSize: [41, 41], // Size of the icon (width, height)
-  iconAnchor: [Math.floor(25 / 2), 41], // Automatic horizontal centering, bottom-aligned vertically
-  popupAnchor: [0, -41] // Popup anchor point (top-left)
+  iconUrl: locationp,
+  iconSize: [41, 41],
+  iconAnchor: [Math.floor(25 / 2), 41],
+  popupAnchor: [0, -41] 
 });
 
 const MyMap = ({ location, error }) => {
   const [selectedPosition, setSelectedPosition] = useState(null);
   const [distance, setDistance] = useState(null);
-
-  useEffect(() => {
-    if (location && selectedPosition) {
-      // Calculate distance between current location and selected position
-      const distanceInKm = calculateDistance(location, selectedPosition);
-      setDistance(distanceInKm);
-    }
-  }, [location, selectedPosition]);
-
-  // Function to calculate distance between two points using Haversine formula
   const calculateDistance = (point1, point2) => {
-    const R = 6371; // Radius of the Earth in km
+    const R = 6371; 
     const lat1 = point1.lat;
     const lon1 = point1.lng;
     const lat2 = point2.lat;
@@ -39,11 +29,21 @@ const MyMap = ({ location, error }) => {
       Math.sin(dLat / 2) * Math.sin(dLat / 2) +
       Math.cos(deg2rad(lat1)) * Math.cos(deg2rad(lat2)) * Math.sin(dLon / 2) * Math.sin(dLon / 2);
     const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
-    const d = R * c; // Distance in km
+    const d = R * c; 
     return d;
   };
 
-  // Function to convert degrees to radians
+  useEffect(() => {
+    if (location && selectedPosition) {
+      const distanceInKm = calculateDistance(location, selectedPosition);
+      setDistance(distanceInKm);
+    }
+  }, [location, selectedPosition]);
+
+
+ 
+
+  
   const deg2rad = (deg) => {
     return deg * (Math.PI / 180);
   };
@@ -52,16 +52,14 @@ const MyMap = ({ location, error }) => {
     <div style={{ height: '100%', width: '100%' }}>
       {location ? (
         <MapContainer
-          center={[location.lat, location.lng]} // Center the map on the user's location
-          zoom={20} // Zoom level
+          center={[location.lat, location.lng]}
+          zoom={20} 
           style={{ height: '90%', width: '80%', margin: 'auto' }}
         >
           <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
-          {/* Render marker for the user's location */}
           <Marker position={[location.lat, location.lng]} icon={targetIcon}>
             <Popup>Your current position</Popup>
           </Marker>
-          {/* Render marker and polyline for the selected position */}
           {selectedPosition && (
             <div>
               <Marker position={[selectedPosition.lat, selectedPosition.lng]} icon={targetIcon}>
@@ -73,25 +71,21 @@ const MyMap = ({ location, error }) => {
               />
             </div>
           )}
-          {/* Component to handle map click events and update selected position */}
           <MapClickHandler setSelectedPosition={setSelectedPosition} />
         </MapContainer>
       ) : (
         <h1>Waiting...</h1>
       )}
-      {/* Display distance if available */}
       {distance && <p>Distance: {distance.toFixed(2)} km</p>}
-      {/* Display error message if any */}
       {error && <p>Error: {error}</p>}
     </div>
   );
 };
 
-// Component to handle map click events and update selected position
+
 const MapClickHandler = ({ setSelectedPosition }) => {
   const map = useMapEvents({
     click: (event) => {
-      // Get position of clicked point
       setSelectedPosition({
         lat: event.latlng.lat,
         lng: event.latlng.lng
@@ -99,7 +93,7 @@ const MapClickHandler = ({ setSelectedPosition }) => {
     },
   });
 
-  return null; // Return null because no component is rendered
+  return null;
 };
 
 export default MyMap;
