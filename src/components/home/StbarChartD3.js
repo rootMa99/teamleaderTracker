aaebcc8f78111870b7c882ee3e11fa09fdbbb2f1
rootMa99 <anasszeroual09@gmail.com>
2024-06-08 15:27:25 +0100

    sd

--- src/components/home/StbarChartD3.js
+++ src/components/home/StbarChartD3.js
@@ -1,10 +1,12 @@
-import React, { useRef, useEffect } from "react";
-import * as d3 from "d3";
+import React, { useRef, useEffect } from 'react';
+import * as d3 from 'd3';
 
-const StbarChartD3 = ({ data }) => {
+const StackedBarChart = ({ data }) => {
   const svgRef = useRef();
 
   useEffect(() => {
+    console.log(data); // Add this line to check if data is received correctly
+
     if (!data) return;
 
     const svg = d3.select(svgRef.current);
@@ -13,57 +15,47 @@ const StbarChartD3 = ({ data }) => {
     const width = 600 - margin.left - margin.right;
     const height = 400 - margin.top - margin.bottom;
 
-    const x = d3
-      .scaleBand()
-      .domain(data.map((d) => d.month))
+    const x = d3.scaleBand()
+      .domain(data.map(d => d.month))
       .range([margin.left, width - margin.right])
       .padding(0.1);
 
-    const y = d3
-      .scaleLinear()
-      .domain([0, d3.max(data, (d) => d3.sum(d.crews, (c) => c.ratio))])
-      .nice()
+    const y = d3.scaleLinear()
+      .domain([0, d3.max(data, d => d3.sum(d.crews, c => c.ratio))]).nice()
       .range([height - margin.bottom, margin.top]);
 
-    const color = d3
-      .scaleOrdinal()
-      .domain(data[0].crews.map((d) => d.crew))
+    const color = d3.scaleOrdinal()
+      .domain(data[0].crews.map(d => d.crew))
       .range(d3.schemeCategory10);
 
     svg.selectAll("*").remove();
 
-    svg
-      .append("g")
+    svg.append("g")
       .selectAll("g")
-      .data(
-        d3.stack().keys(data[0].crews.map((d) => d.crew))(
-          data.map((d) => d.crews)
-        )
-      )
-      .enter()
-      .append("g")
+      .data(d3.stack().keys(data[0].crews.map(d => d.crew))(data.map(d => d.crews)))
+      .enter().append("g")
       .attr("fill", (d, i) => color(i))
       .selectAll("rect")
-      .data((d) => d)
-      .enter()
-      .append("rect")
+      .data(d => d)
+      .enter().append("rect")
       .attr("x", (d, i) => x(data[i].month))
-      .attr("y", (d) => y(d[1]))
-      .attr("height", (d) => y(d[0]) - y(d[1]))
+      .attr("y", d => y(d[1]))
+      .attr("height", d => y(d[0]) - y(d[1]))
       .attr("width", x.bandwidth());
 
-    svg
-      .append("g")
+    svg.append("g")
       .attr("transform", `translate(0,${height - margin.bottom})`)
       .call(d3.axisBottom(x));
 
-    svg
-      .append("g")
+    svg.append("g")
       .attr("transform", `translate(${margin.left},0)`)
       .call(d3.axisLeft(y));
+
   }, [data]);
 
-  return <svg ref={svgRef} style={{ height: "100%", width: "100%" }}></svg>;
+  return (
+    <svg ref={svgRef} width="600" height="400"></svg>
+  );
 };
 
-export default StbarChartD3;
+export default StackedBarChart;
