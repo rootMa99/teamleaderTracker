6948297aeeb288267a2d61b2042b6ee0df95ecbf
rootMa99 <anasszeroual09@gmail.com>
2024-06-08 15:05:01 +0100

    sdd

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ import MyMap from "./components/home/MyMap.js";
 import StackedBarChart from "./components/home/StackedBarChart";
 import NavBar from "./components/ui/NavBar";
 import MapChart from "./components/home/MapChart.js";
+import StackedBarCharts from "./components/home/StackedBarCharts.js";
 
 function App() {
   const data = [
@@ -24,6 +25,79 @@ function App() {
     { label: "BC", series1: 11, series2: 35, series3: 25 },
     { label: "BD", series1: 17, series2: 5, series3: 3 },
   ];
+  const stackedD=[
+    {
+      month:"01",
+      crews:[
+        {crew:"k1", ratio:2879},
+        {crew:"k2", ratio:3889},
+        {crew:"k3", ratio:4856},
+        {crew:"k4", ratio:5856},
+        {crew:"k5", ratio:6570},
+        {crew:"k6", ratio:2879},
+        {crew:"kÈ", ratio:7879},
+        {crew:"k1s", ratio:6845},
+        {crew:"ksd", ratio:2879},
+        {crew:"kzz", ratio:5879},
+        {crew:"kee", ratio:9879},
+        {crew:"kèç", ratio:6879},
+        {crew:"ké&", ratio:5879},
+        {crew:"kvb", ratio:8879},
+        {crew:"k89", ratio:2879},
+        {crew:"k88", ratio:1879},
+        {crew:"k77", ratio:879},
+        {crew:"k4G", ratio:8879},
+        {crew:"k2", ratio:1879},
+        {crew:"k1UI", ratio:3879},
+        {crew:"k88", ratio:8979},
+        {crew:"k00", ratio:2379},
+        {crew:"kNK", ratio:9079},
+        {crew:"k221", ratio:6679},
+        {crew:"kYU", ratio:7779},
+        {crew:"kNK", ratio:4479},
+        {crew:"k1PO", ratio:1179},
+        {crew:"k1NJS8", ratio:3379},
+        {crew:"k1NJ9", ratio:7279},
+        {crew:"k1NN", ratio:1389},
+      ]
+    },
+    {
+      month:"02",
+      crews:[
+        {crew:"k1", ratio:2879},
+        {crew:"k2", ratio:3889},
+        {crew:"k3", ratio:4856},
+        {crew:"k4", ratio:5856},
+        {crew:"k5", ratio:6570},
+        {crew:"k6", ratio:2879},
+        {crew:"kÈ", ratio:7879},
+        {crew:"k1s", ratio:6845},
+        {crew:"ksd", ratio:2879},
+        {crew:"kzz", ratio:5879},
+        {crew:"kee", ratio:9879},
+        {crew:"kèç", ratio:6879},
+        {crew:"ké&", ratio:5879},
+        {crew:"kvb", ratio:8879},
+        {crew:"k89", ratio:2879},
+        {crew:"k88", ratio:1879},
+        {crew:"k77", ratio:879},
+        {crew:"k4G", ratio:8879},
+        {crew:"k2", ratio:1879},
+        {crew:"k1UI", ratio:3879},
+        {crew:"k88", ratio:8979},
+        {crew:"k00", ratio:2379},
+        {crew:"kNK", ratio:9079},
+        {crew:"k221", ratio:6679},
+        {crew:"kYU", ratio:7779},
+        {crew:"kNK", ratio:4479},
+        {crew:"k1PO", ratio:1179},
+        {crew:"k1NJS8", ratio:3379},
+        {crew:"k1NJ9", ratio:7279},
+        {crew:"k1NN", ratio:1389},
+      ]
+    },
+    
+  ]
   const [location, setLocation] = useState(null);
   const [error, setError] = useState(null);
   const getLocation = () => {
@@ -69,6 +143,9 @@ function App() {
       </div>
       <div style={{ height: "30rem", backgroundColor: "#3d7558" }}>
         <MapChart />
+     </div>
+     <div style={{ height: "30rem", backgroundColor: "#3d7558" }}>
+        <StackedBarCharts data={stackedD} />
      </div>
     </div>
   );
---
+++ src/components/home/StackedBarCharts.js
@@ -0,0 +1,102 @@
+import React from "react";
+import { Bar } from "react-chartjs-2";
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  BarElement,
+  Title,
+  Tooltip,
+  Legend,
+} from "chart.js";
+import ChartDataLabels from "chartjs-plugin-datalabels";
+
+ChartJS.register(
+  CategoryScale,
+  LinearScale,
+  BarElement,
+  Title,
+  Tooltip,
+  Legend,
+  ChartDataLabels
+);
+
+const colors = [
+  "#006B63",
+  "#FF6384",
+  "#36A2EB",
+  "#FFCE56",
+  "#4BC0C0",
+  "#9966FF",
+  "#FF9F40",
+  "#FFCD56",
+  "#4D5360",
+  "#FF6384",
+  "#36A2EB",
+  "#FFCE56",
+];
+
+const StackedBarCharts = ({ data }) => {
+  const chartData = {
+    labels: data.map((m) => m.month),
+    datasets: data.flatMap((m, monthIndex) =>
+      m.crews.map((crew, crewIndex) => ({
+        label: `${m.month} - ${crew.crew}`,
+        data: data.map((monthData) => crew.ratio),
+        backgroundColor: colors[crewIndex % colors.length],
+      }))
+    ),
+  };
+
+  const options = {
+    responsive: true,
+    maintainAspectRatio: false,
+    scales: {
+      x: {
+        stacked: false,
+        ticks: {
+          color: "white",
+          font: {
+            weight: "bold",
+          },
+        },
+      },
+      y: {
+        stacked: false,
+        grid: {
+          color: "#f3f3f34f",
+        },
+        ticks: {
+          display: typeof data.home === "undefined" ? false : true,
+          color: "white",
+          font: {
+            weight: "bold",
+          },
+        },
+      },
+    },
+    plugins: {
+      legend: {
+        labels: {
+          color: "#FAF0E6",
+        },
+        display: true,
+      },
+      datalabels: {
+        display: true,
+        color: "#FFFAD7",
+        font: {
+          weight: "bold",
+          size: 12,
+        },
+        formatter: (value) => value.toFixed(0),
+        anchor: "end",
+        align: "start",
+      },
+    },
+  };
+
+  return <Bar data={chartData} options={options} />;
+};
+
+export default StackedBarCharts;
